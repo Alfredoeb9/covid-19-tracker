5606e18eb731780c81892eac8884fd9cee16ea72
Alfredoeb9 <alfredoeb96@gmail.com>
2020-08-26 21:10:44 -0700

    initial commit

---
+++ src/firebase.js
@@ -0,0 +1,10 @@
+const firebaseConfig = {
+  apiKey: "AIzaSyC-OBmCSRMO99ZUPrc3wspa3rXhB28TO8Q",
+  authDomain: "covid-19-tracker-4e5d4.firebaseapp.com",
+  databaseURL: "https://covid-19-tracker-4e5d4.firebaseio.com",
+  projectId: "covid-19-tracker-4e5d4",
+  storageBucket: "covid-19-tracker-4e5d4.appspot.com",
+  messagingSenderId: "723174973901",
+  appId: "1:723174973901:web:c78c22164a4a1fc2786484",
+  measurementId: "G-BX4S0VNHJE"
+};
