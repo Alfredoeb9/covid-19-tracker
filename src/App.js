a94a9009becb3b3114a283d493ab40c550218ce4
Alfredoeb9 <alfredoeb96@gmail.com>
2020-08-30 00:02:58 -0700

    built overall structure of app

--- src/App.js
+++ src/App.js
@@ -4,7 +4,11 @@ import  {
   MenuItem,
   FormControl,
   Select,
+  Card,
+  CardContent
 } from "@material-ui/core";
+import InfoBox from './InfoBox';
+import Map from './Map';
 
 function App() {
   const [countries, setCountries] = useState([]);
@@ -40,30 +44,44 @@ function App() {
 
   return (
     <div className="app">
+      <div className="app_left">
+        <div className="app_header">
+          <h1>COVID-19 TRACKER</h1>
 
-      <h1>COVID-19 TRACKER</h1>
+          {/* Material UI library */}
+          <FormControl className="app_dropdown">
+            <Select variant="outlined" onChange={onCountryChange} value={country}>
+              <MenuItem value="worldwide">Worldwide</MenuItem>
+              {countries.map((country) => (
+                <MenuItem value={country.value}>{country.name}</MenuItem>
+              ))}
+            </Select>
+          </FormControl>
+        </div>
+          {/* Header */}
 
-      {/* Material UI library */}
-      <FormControl className="app_dropdown">
-        <Select variant="outlined" onChange={onCountryChange} value={country}>
-          <MenuItem value="worldwide">Worldwide</MenuItem>
-          {countries.map((country) => (
-            <MenuItem value={country.value}>{country.name}</MenuItem>
-          ))}
-        </Select>
-      </FormControl>
-      {/* Header */}
+          {/* Title + Select iinput dropdown field */}
 
-      {/* Title + Select iinput dropdown field */}
+          <div className="app_stats">
+            <InfoBox title="Coronavirus Cases" cases={2334} total={2000}/>
 
-      {/* InfoBoxs */}
-      {/* InfoBoxs */}
-      {/* InfoBoxs */}
+            <InfoBox title="Recovered" cases={4393} total={3000}/>
 
-      {/* Table */}
-      {/* Graph */}
+            <InfoBox title="Deaths" cases={12121} total={4000}/>
+          </div>
 
-      {/* Map */}
+        <Map />
+      </div>
+      <Card className="app_right">
+        <CardContent>
+          <h3>Live Cases by Country</h3>
+
+          <h3>WorldWide by Cases</h3>
+        </CardContent>
+          {/* Table */}
+          {/* Graph */}
+      </Card>
+      
     </div>
   );
 }
