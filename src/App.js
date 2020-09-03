47d17588d13dc8174d177c6692e0ba841d4b4f8a
Alfredoeb9 <alfredoeb96@gmail.com>
2020-09-02 21:26:11 -0700

    added leaflet api

--- src/App.js
+++ src/App.js
@@ -9,14 +9,30 @@ import  {
 } from "@material-ui/core";
 import InfoBox from './InfoBox';
 import Map from './Map';
+import Table from './Table';
+import { sortData } from './utility';
+import LineGraph from './LineGraph';
+import 'leaflet/dist/leaflet.css';
 
 function App() {
   const [countries, setCountries] = useState([]);
   const [country, setCountry] = useState('worldwide');
-
-  // https://disease.sh/v3/covid-19/countries
+  const [countryInfo, setCountryInfo] = useState({});
+  const [tableData, setTableData] = useState([]);
+  const [mapCenter, setMapCenter] = useState({ lat: 34.80746, lng: -40.4796 });
+  const [mapZoom, setMapZoom] = useState(3);
+  
 
   // When the page loads fire off useEffect
+  useEffect(() => {
+    fetch('https://disease.sh/v3/covid-19/all')
+      .then(response => response.json())
+      .then(data => {
+        setCountryInfo(data);
+      });
+  }, []);
+
+
   useEffect(() => {
     const getCountriesData = async () => {
       await fetch("https://disease.sh/v3/covid-19/countries")
@@ -28,7 +44,9 @@ function App() {
               value: country.countryInfo.iso2 // UI, USA, FR
             }
           ));
-
+          
+          const sortedData = sortData(data);
+          setTableData(sortedData);
           setCountries(countries);
         });
     };
@@ -40,6 +58,16 @@ function App() {
     const countryCode = event.target.value;
     setCountry(countryCode);
 
+    const url = countryCode === 'worldwide' ? 'https://disease.sh/v3/covid-19/all' : `https://disease.sh/v3/covid-19/countries/${countryCode}`;
+
+    await fetch(url)
+      .then(response => response.json())
+      .then(data => {
+        setCountry(countryCode);
+
+        // All of the data from the countries reponse
+        setCountryInfo(data);
+      });
   };
 
   return (
@@ -63,22 +91,22 @@ function App() {
           {/* Title + Select iinput dropdown field */}
 
           <div className="app_stats">
-            <InfoBox title="Coronavirus Cases" cases={2334} total={2000}/>
+            <InfoBox title="Coronavirus Cases" cases={countryInfo.todayCases} total={countryInfo.cases}/>
 
-            <InfoBox title="Recovered" cases={4393} total={3000}/>
+            <InfoBox title="Recovered" cases={countryInfo.todayRecovered} total={countryInfo.recovered}/>
 
-            <InfoBox title="Deaths" cases={12121} total={4000}/>
+            <InfoBox title="Deaths" cases={countryInfo.todayDeaths} total={countryInfo.deaths}/>
           </div>
 
-        <Map />
+        <Map center={mapCenter} zoom={mapZoom}/>
       </div>
       <Card className="app_right">
         <CardContent>
           <h3>Live Cases by Country</h3>
-
+          <Table countries={tableData} />
           <h3>WorldWide by Cases</h3>
+          <LineGraph />
         </CardContent>
-          {/* Table */}
           {/* Graph */}
       </Card>
       
